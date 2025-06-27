b9a6a32749d3ed72af7fa5674a2d27080d6b1f4e
gyusummer <gyusummer@gmail.com>
2025-06-27 13:54:11 +0900

    [손 정보 적용중]
    현재 매우 기괴하게 덮어쓰기 가능

---
+++ Assets/#IKFigure/FigureHandPose.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR.Hands;
+
+public class FigureHandPose : MonoBehaviour
+{
+    //public XRHandSubsystem m_HandSubsystem;
+    public XRHandTrackingEvents m_HandTrackingEvents;
+    public Transform[] m_LeftHandJointTransforms;
+    private List<Pose> m_LeftHandJointPoses = new List<Pose>();
+    public bool m_isReserved = false;
+    
+    void Start()
+    {
+        // var handSubsystems = new List<XRHandSubsystem>();
+        // SubsystemManager.GetSubsystems(handSubsystems);
+        //
+        // for (var i = 0; i < handSubsystems.Count; ++i)
+        // {
+        //     var handSubsystem = handSubsystems[i];
+        //     if (handSubsystem.running)
+        //     {
+        //         m_HandSubsystem = handSubsystem;
+        //         break;
+        //     }
+        // }
+        //
+        // if (m_HandSubsystem != null)
+        //     m_HandSubsystem.updatedHands += OnUpdatedHands;
+        m_HandTrackingEvents.jointsUpdated.AddListener(OnJointsUpdated);
+    }
+
+    // private void OnUpdatedHands(XRHandSubsystem subsystem,
+    //     XRHandSubsystem.UpdateSuccessFlags updateSuccessFlags,
+    //     XRHandSubsystem.UpdateType updateType)
+    // {
+    //     switch (updateType)
+    //     {
+    //         case XRHandSubsystem.UpdateType.Dynamic:
+    //             // Update game logic that uses hand data
+    //             break;
+    //         case XRHandSubsystem.UpdateType.BeforeRender:
+    //             // Update visual objects that use hand data
+    //             if (m_isReserved)
+    //             {
+    //                 UpdateJointsData(subsystem.leftHand);
+    //                 ApplyPoseData();
+    //                 m_isReserved = false;
+    //             }
+    //             break;
+    //     }
+    // }
+
+    private void OnJointsUpdated(XRHandJointsUpdatedEventArgs args)
+    {
+        UpdateJointsData(args);
+        ApplyPoseData();
+        m_isReserved = false;
+    }
+    
+    private void UpdateJointsData(XRHandJointsUpdatedEventArgs args)
+    {
+        m_LeftHandJointPoses.Clear();
+        for(var i = XRHandJointID.ThumbMetacarpal.ToIndex();
+            i < XRHandJointID.EndMarker.ToIndex();
+            i++)
+        {
+            if (i is 7 or 12 or 17 or 22)
+            {
+                // Skip the metacarpal joints for the index, middle, ring, and little fingers
+                continue;
+            }
+            var trackingData = args.hand.GetJoint(XRHandJointIDUtility.FromIndex(i));
+
+            if (trackingData.TryGetPose(out Pose pose))
+            {
+                // 데이터는 26개 본은 20개
+                m_LeftHandJointPoses.Add(pose);
+            }
+        }
+    }
+
+    private void ApplyPoseData()
+    {
+        for (int i = 0; i < m_LeftHandJointTransforms.Length; i++)
+        {
+            m_LeftHandJointTransforms[i].SetLocalPose(m_LeftHandJointPoses[i]);
+        }
+    }
+}
