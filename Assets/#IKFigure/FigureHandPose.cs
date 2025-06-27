9046fb762771c9295e3e17f1338a3e96edd386f1
gyusummer <gyusummer@gmail.com>
2025-06-27 17:59:09 +0900

    [디버그 띄움]

--- Assets/#IKFigure/FigureHandPose.cs
+++ Assets/#IKFigure/FigureHandPose.cs
@@ -15,46 +15,19 @@ public class FigureHandPose : MonoBehaviour
     private List<Pose> m_LeftHandJointPoses = new List<Pose>();
     public bool m_isReserved = false;
     
+    
+    // Debug Joint
+    public GameObject jointPrefab;
+    public Transform[] debugJoints = new Transform[26];
+    
     void Start()
     {
-        // var handSubsystems = new List<XRHandSubsystem>();
-        // SubsystemManager.GetSubsystems(handSubsystems);
-        //
-        // for (var i = 0; i < handSubsystems.Count; ++i)
-        // {
-        //     var handSubsystem = handSubsystems[i];
-        //     if (handSubsystem.running)
-        //     {
-        //         m_HandSubsystem = handSubsystem;
-        //         break;
-        //     }
-        // }
-        //
-        // if (m_HandSubsystem != null)
-        //     m_HandSubsystem.updatedHands += OnUpdatedHands;
         m_HandTrackingEvents.jointsUpdated.AddListener(OnJointsUpdated);
+        for (int i = 0; i < debugJoints.Length; i++)
+        {
+            debugJoints[i] = Instantiate(jointPrefab).transform;
+        }
     }
-
-    // private void OnUpdatedHands(XRHandSubsystem subsystem,
-    //     XRHandSubsystem.UpdateSuccessFlags updateSuccessFlags,
-    //     XRHandSubsystem.UpdateType updateType)
-    // {
-    //     switch (updateType)
-    //     {
-    //         case XRHandSubsystem.UpdateType.Dynamic:
-    //             // Update game logic that uses hand data
-    //             break;
-    //         case XRHandSubsystem.UpdateType.BeforeRender:
-    //             // Update visual objects that use hand data
-    //             if (m_isReserved)
-    //             {
-    //                 UpdateJointsData(subsystem.leftHand);
-    //                 ApplyPoseData();
-    //                 m_isReserved = false;
-    //             }
-    //             break;
-    //     }
-    // }
 
     private void OnJointsUpdated(XRHandJointsUpdatedEventArgs args)
     {
@@ -69,22 +42,24 @@ public class FigureHandPose : MonoBehaviour
         
         Pose rootPose = args.hand.rootPose;
         
-        for(var i = XRHandJointID.ThumbMetacarpal.ToIndex();
+        for(var i = XRHandJointID.BeginMarker.ToIndex();
             i < XRHandJointID.EndMarker.ToIndex();
             i++)
         {
-            if (i is 7 or 12 or 17 or 22)
-            {
-                // Skip the metacarpal joints for the index, middle, ring, and little fingers
-                continue;
-            }
             var trackingData = args.hand.GetJoint(XRHandJointIDUtility.FromIndex(i));
-
             if (trackingData.TryGetPose(out Pose pose))
             {
-                // 데이터는 26개 본은 20개
                 pose.position = pose.position - rootPose.position;
                 pose.rotation = Quaternion.Inverse(rootPose.rotation) * pose.rotation;
+                
+                debugJoints[i].position = Quaternion.AngleAxis(-90f, m_ModelLeftHandRootTransform.up) * pose.position + m_ModelLeftHandRootTransform.position;
+                debugJoints[i].rotation = m_ModelLeftHandRootTransform.rotation * pose.rotation;
+                
+                if (i is 1 or 2 or 6 or 11 or 16 or 21)
+                {
+                    // Skip the metacarpal joints for the index, middle, ring, and little fingers
+                    continue;
+                }
                 m_LeftHandJointPoses.Add(pose);
             }
         }
